e3da01ce529af3b7498f4b67689878f072c2ad20
Carlos Otavio <otavio.ads@gmail.com>
2021-05-12 20:54:27 -0300

    categoria nova

--- src/main/java/com/carlos/springvscode/controller/HelloController.java
+++ src/main/java/com/carlos/springvscode/controller/HelloController.java
@@ -1,15 +1,33 @@
 package com.carlos.springvscode.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.carlos.springvscode.domain.Categoria;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @RestController
+//@RequestMapping(value = "/Categorias")
 public class HelloController {
-    @GetMapping(value="/")
-    public String getMethodName() {
-        return  "Hello Controller!";
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> getMethodName() {
+
+
+        Categoria cat1 = new Categoria(1, "nome");
+        Categoria cat2 = new Categoria(2, "carlos");
+        List<Categoria> lCat = new ArrayList<>();
+        lCat.add(cat1);
+        lCat.add(cat2);
+
+        return  lCat;
     }
+    
     
     
 }
