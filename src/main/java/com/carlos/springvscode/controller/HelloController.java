059e300520e04c1f555fe9605b30bb5a1a385995
Carlos Otavio <otavio.ads@gmail.com>
2021-05-12 19:41:43 -0300

    projeto vs-code sprng-boot

---
+++ src/main/java/com/carlos/springvscode/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.carlos.springvscode.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+    @GetMapping(value="/")
+    public String getMethodName() {
+        return  "Hello Controller!";
+    }
+    
+    
+}
